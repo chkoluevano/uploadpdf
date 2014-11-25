964f4f9a6d9894a2a6d3abcf7f5483341135ff38
CHKO <sluevano@gmail.com>
2014-11-24 19:08:05 -0600

    modificacion ok

--- js/main.js
+++ js/main.js
@@ -4,39 +4,62 @@ $(document).ready(function(){
 		});
 	    
 	    $('#botonenviar').on('click',function(){
-	    	var formulario = new FormData($("#frmCampana")[0]);  
-	    	var val = $('#imagen').val();
-	    	var ext=val.substring(val.lastIndexOf('.') + 1);
-	    	if (ext=="pdf"){
-		    	$('#loader').html("<img src='img/loader.gif'>");
-		    	$('#mensaje').html("Cargando el archivo, no cierres el navegador");
-		    	$.ajax({
-		            url:'upload.php',
-		            type:'POST', 
-		            data:formulario,
-		            cache:false,
-		            contentType:false,                    
-		            processData:false,
-		         	success: function(datos){ 
-		         		if(String(datos)!="ERROR" || String(datos)==""){ 
-		            		//UpdateParse(datos);
-		            		$('#mensaje').html(datos);
-		      
-		            	}
-		            	else{
-		            		$('#mensaje').html(datos);
-		            		$('#loader').html("");
-		            	}
-		            	
-		            }
-		   	 	}); 
+	    	//var formulario = new FormData($("#frmCampana")[0]); 
+	    	var formulario = new FormData(); 
+	    	formulario.append("portadas", $('#portadas')[0].files[0]);
+	    	formulario.append("notas", $('#notas')[0].files[0]);
+
+
+	    	var inputs=[$('#portadas'),$('#notas')];
+	    	var blancos=0;
+	    	var _break=false;
+			for (i=1;i<=inputs.length;i++){
+				var valor = inputs[i-1].val();
+				var ext = valor.substring(valor.lastIndexOf('.') + 1);
+				if(ext==""){
+					blancos++;
+				}
+				else{
+					if(ext!="pdf"){
+						_break=true;
+						$('#mensaje').html("Alerta: El archivo a cargar debe ser PDF");
+						break;
+					}
+				}
 			}
-			else{
-				$('#mensaje').html("Alerta: El archivo a cargar debe ser PDF");
-				$('#loader').html("");
+			if(!_break){
+				if(blancos>1){
+					$('#mensaje').html("Se debe subir por lo menos 1 archivo");
+				}
+				else{
+					$('#loader').html("<img src='img/loader.gif'>");
+		    		$('#mensaje').html("Cargando el archivo, no cierres el navegador");
+			    	$.ajax({
+			            url:'upload.php',
+			            type:'POST', 
+			            data:formulario,
+			            cache:false,
+
+			            contentType:false,                    
+			            processData:false,
+			         	success: function(data){ 
+			         		$('#mensaje').html('');
+			            	var response = $.parseJSON(data);
+			            	//debugger;
+			            	response.forEach(function(r){
+			            		$('#mensaje').append("El archivo de " + r.tipo + " " + r.status + "<br>");
+			            	});
+			            	$('#loader').html("");
+			            	
+			            	
+			            }
+			   	 	}); 
+				}
 			}
     	});
+
 });
+
 
 function UpdateParse(elemento){
 	$('#mensaje').html("Enviando archivo a la APP, no cierres el navegador");
@@ -58,3 +81,6 @@ function UpdateParse(elemento){
 	  }
 	});
 }
+
+
+
